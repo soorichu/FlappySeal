379bffa01f45a20edbb8053cd869d2db8a1a2605
Soori <soorichu@gmail.com>
2021-09-13 15:27:52 +0900

    not yet complete

---
+++ Assets/Scripts/ScrollObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float startPosition;
+    public float endPosition;
+
+    void Update()
+    {
+        transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+
+        if(transform.position.x <= endPosition)
+        {
+            ScrollEnd();
+        }
+    }
+
+    void ScrollEnd()
+    {
+        transform.Translate(-1 * (endPosition - startPosition), 0, 0);
+        SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+    }
+}
---
+++ Assets/Scripts/SealController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SealController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+
+    public float maxHeight;
+    public float flapVelocity;
+    public bool isDead = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && transform.position.y < maxHeight)
+        {
+            Flap();
+        }
+    }
+
+    public void Flap()
+    {
+        if (isDead || rb.isKinematic)
+        {
+            return;
+        }
+
+        rb.velocity = new Vector2(0.0f, flapVelocity);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isDead) return;
+        isDead = true;
+        animator.SetBool("flap", false);
+    }
+
+
+}
